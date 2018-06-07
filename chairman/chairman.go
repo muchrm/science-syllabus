a91b1e476deb03ad0444474f33bb9824f0e2cd99
Pongpanot Chuaysakun <muchrm.me@gmail.com>
2018-06-07 11:51:13 +0700

    update chairman

--- chairman/chairman.go
+++ chairman/chairman.go
@@ -19,6 +19,7 @@ func FromExcel(db *mongo.Database, sheet *xlsx.Sheet) {
 		if len(row.Cells[0].String()) > 0 {
 			text := util.RemovePrefix(row.Cells[1].String(), config.GetPrefixs())
 			name := strings.Split(text, " ")
+			fmt.Println(text, name)
 			teacher, notExist := teacher.TeacherNotExist(db, teacher.Teacher{OfficerName: name[0], OfficerSurname: strings.Join(name[1:], " ")})
 			if notExist == true {
 				log.Println(fmt.Sprintf("'%s' '%s' not a teacher", name[0], name[1]))
